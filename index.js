8f57ea6d6549b16b9f1ffb6e3224b5bf83e6d016
dbsasser <danielbsasser@gmail.com>
2020-05-28 13:18:26 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map( title => {
+    let words = title.split(" ")
+    let titleCase = words.map( word => word.charAt(0).toUpperCase() + word.slice(1) )
+    let response = titleCase.join(" ")
+    return response
+  })
 }
